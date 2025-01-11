9a7df88a9df2648321e97ca6c428641e718b7d7a
tder <1218546874@qq.com>
2025-01-12 00:30:50 +0800

    add ref; update push

--- push.cpp
+++ push.cpp
@@ -4,10 +4,10 @@ using namespace std;
 void run(string x) {
 	cout<<x<<endl; system(x.c_str());
 }
-signed main() {
+signed main(signed argc, char** argv) {
 	freopen("commit.log", "r", stdin);
 	string s; getline(cin, s);
-	run("git add .");
-	run("git commit -m \"" + s + "\"");
-	run("git push");
+	string t; if(argc > 1) t = argv[1];
+	if(t != "p") run("git add ."), run("git commit -m \"" + s + "\"");
+	if(t != "c") run("git push");
 }
